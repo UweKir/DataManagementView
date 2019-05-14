f91dfb84f70679410a4fb4ecb75aaaf367c3e671
UweKir <uwe.kirsch@scancoin.com>
2019-05-14 08:12:45 +0200

    2019-05-14

--- WebApplication/ConsoleApp1/Program.cs
+++ WebApplication/ConsoleApp1/Program.cs
@@ -11,99 +11,144 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Decimal runTime = 0m;
-
-            Decimal es16_22 = 0m;
-            Decimal es2_8 = 0m;
-            Decimal es8_16 = 0m;
-            Decimal es8_22 = 0m;
-            Decimal p40_100 = 0m;
-
-            Decimal brecher = 0m;
-            Decimal vorsieb = 0m;
-            Decimal brechsand = 0m;
-            Decimal abfall = 0m;
-
-            Decimal deponie = 0m;
-
-            Decimal totalEdelSplit = 0m;
 
 
+            DateTime beginDate = DateTime.Parse("2018-04-02");
+            DateTime endDate = DateTime.Parse("2019-06-25");
 
+            List<WebApplication.Models.UsageDetail> lstUsage = new List<WebApplication.Models.UsageDetail>();
 
             using (KTBDataManagerEntities context = new KTBDataManagerEntities())
             {
-                var prodTime = from p in context.view_dailyProduction
-                           where p.UNIT.ToLower() == "h"
-                           group p by new { p.UNIT } into g
-                           select new
-                           {
-                               Unit = g.Key.UNIT,
-                               Sum = g.Sum(p=>p.VALUE)
-                              
-                           };
-                foreach (var pt in prodTime)
+                var s = context.sp_GetConsumerCounterDifference(beginDate, endDate, null, null, null, null);
+
+                foreach(var g in s)
                 {
-                    runTime = (Decimal) pt.Sum;
+                    lstUsage.Add(new WebApplication.Models.UsageDetail() { Device = g.DEVICE, Value = (Decimal)g.USAGE });
+                   
                 }
 
-                var prodTons = from p in context.view_dailyProduction
-                               where p.UNIT.ToLower() == "t"
-                               group p by new { p.ARTICLE } into g
+                /*
+                var prodTons = from p in context.sp_GetConsumerCounterDifference(beginDate.Date, endDate.Date, "", "", "","","")
+                               where p>= beginDate.Date && p.use_date <= endDate.Date
                                select new
                                {
-                                   Article = g.Key.ARTICLE,
-                                   Sum = g.Sum(p => p.VALUE)
+                                   
+                                   Unit = p.UNIT,
+                                   Value = p.VALUE,
+                                   Location = p.LOCATION,
+                                   Device = p.DEVICE
 
                                };
-                foreach (var art in prodTons)
+
+                foreach (var prod in prodTons)
                 {
-                    Decimal v = (Decimal)art.Sum;
-
-                    switch(art.Article)
-                    {
-                        case "16/22":
-                            es16_22 = v;
-                            totalEdelSplit += v;
-                            break;
-                        case "2/8":
-                            es2_8 = v;
-                            totalEdelSplit += v;
-                            break;
-                        case "8/16":
-                            es8_16 = v;
-                            totalEdelSplit += v;
-                            break;
-                        case "8/22":
-                            es8_22 = v;
-                            totalEdelSplit += v;
-                            break;
-                        case "0/2":
-                            brechsand = v;
-                            break;
-                        case "0/80":
-                            vorsieb = v;
-                            break;
-                        case "40/100":
-                            p40_100 = v;
-                            break;
-                        case "0/200":
-                            brecher = v;
-                            break;
-                        default:
-                            continue;
-                      
-                    }
-
-                   
+                    lstUsage.Add(new WebApplication.Models.UsageDetail() { Device = prod.Device, Location = prod.Location, Unit = prod.Unit, Value = (Decimal)prod.Value });
                     
                 }
+                */
+            }
 
-                deponie = vorsieb - p40_100;
-                abfall = (brecher + vorsieb) - totalEdelSplit - brechsand - deponie;
+            
+
+
+
+                /*
+                Decimal runTime = 0m;
+
+                Decimal es16_22 = 0m;
+                Decimal es2_8 = 0m;
+                Decimal es8_16 = 0m;
+                Decimal es8_22 = 0m;
+                Decimal p40_100 = 0m;
+
+                Decimal brecher = 0m;
+                Decimal vorsieb = 0m;
+                Decimal brechsand = 0m;
+                Decimal abfall = 0m;
+
+                Decimal deponie = 0m;
+
+                Decimal totalEdelSplit = 0m;
+
+
+
+
+                using (KTBDataManagerEntities context = new KTBDataManagerEntities())
+                {
+                    var prodTime = from p in context.view_dailyProduction
+                               where p.UNIT.ToLower() == "h"
+                               group p by new { p.UNIT } into g
+                               select new
+                               {
+                                   Unit = g.Key.UNIT,
+                                   Sum = g.Sum(p=>p.VALUE)
+
+                               };
+                    foreach (var pt in prodTime)
+                    {
+                        runTime = (Decimal) pt.Sum;
+                    }
+
+                    var prodTons = from p in context.view_dailyProduction
+                                   where p.UNIT.ToLower() == "t"
+                                   group p by new { p.ARTICLE } into g
+                                   select new
+                                   {
+                                       Article = g.Key.ARTICLE,
+                                       Sum = g.Sum(p => p.VALUE)
+
+                                   };
+                    foreach (var art in prodTons)
+                    {
+                        Decimal v = (Decimal)art.Sum;
+
+                        switch(art.Article)
+                        {
+                            case "16/22":
+                                es16_22 = v;
+                                totalEdelSplit += v;
+                                break;
+                            case "2/8":
+                                es2_8 = v;
+                                totalEdelSplit += v;
+                                break;
+                            case "8/16":
+                                es8_16 = v;
+                                totalEdelSplit += v;
+                                break;
+                            case "8/22":
+                                es8_22 = v;
+                                totalEdelSplit += v;
+                                break;
+                            case "0/2":
+                                brechsand = v;
+                                break;
+                            case "0/80":
+                                vorsieb = v;
+                                break;
+                            case "40/100":
+                                p40_100 = v;
+                                break;
+                            case "0/200":
+                                brecher = v;
+                                break;
+                            default:
+                                continue;
+
+                        }
+
+
+
+                    }
+
+                    deponie = vorsieb - p40_100;
+                    abfall = (brecher + vorsieb) - totalEdelSplit - brechsand - deponie;
+                    
 
 
             }
+            */
         }
     }
 }
--- WebApplication/WebApplication/Controllers/CustomUsageTableController.cs
+++ WebApplication/WebApplication/Controllers/CustomUsageTableController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models;
+
 
 namespace WebApplication.Controllers
 {
@@ -12,6 +14,64 @@ namespace WebApplication.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetCustomUsageTableData(String startDate, String endDate)
+        {
+
+            DateTime dtStart = DateTime.Now;
+            DateTime dtEnd = DateTime.Now;
+
+
+            if (!String.IsNullOrEmpty(startDate))
+            {
+                dtStart = DateTime.ParseExact(startDate,
+                 General.DateTimeFormat,
+                 System.Globalization.CultureInfo.InvariantCulture);
+
+            }
+
+            if (!String.IsNullOrEmpty(endDate))
+            {
+                 dtEnd = DateTime.ParseExact(endDate,
+                 General.DateTimeFormat,
+                 System.Globalization.CultureInfo.InvariantCulture);
+
+            }
+
+            List<WebApplication.Models.UsageDetail> lstUsage = new List<WebApplication.Models.UsageDetail>();
+
+            using (KTBDataManagerEntities context = new KTBDataManagerEntities())
+            {
+                var prodTons = from p in context.view_dailyUsage
+                               where p.use_date >= dtStart.Date && p.use_date <= dtEnd.Date
+                               group p by new { p.DEVICE } into g
+                               select new
+                               {
+                                   Unit = g.Key.DEVICE,
+                                   Value = g.Sum(p => p.VALUE)
+
+                               };
+                /*
+                select new
+                               {
+
+                                   Unit = p.UNIT,
+                                   Value = p.VALUE,
+                                   Location = p.LOCATION,
+                                   Device = p.DEVICE
+
+                               };*/
+
+                foreach (var prod in prodTons)
+                {
+                    lstUsage.Add(new WebApplication.Models.UsageDetail() { Device = prod.Unit, Value = (Decimal)prod.Value });
+
+                }
+            }
+
+            return Json(lstUsage, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
