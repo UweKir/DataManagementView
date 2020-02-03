b52f1eafe04d0485e15abf2050758f77de56445c
UweKir <uwe.kirsch@scancoin.com>
2020-02-03 16:00:50 +0100

    2020-02-03

--- WebApplication/WebApplication/Controllers/DayUsageChartsController.cs
+++ WebApplication/WebApplication/Controllers/DayUsageChartsController.cs
@@ -18,6 +18,7 @@ namespace WebApplication.Controllers
         public JsonResult GetDevices()
         {
             List<ProdDevice> lstDevices = new List<ProdDevice>();
+            lstDevices.Add(new ProdDevice() { Key = "all", Name = "*" });
 
             using (KTBDataManagerEntities context = new KTBDataManagerEntities())
             {
@@ -29,7 +30,7 @@ namespace WebApplication.Controllers
                 {
                     ProdDevice pDevice = new ProdDevice() { Key = dev.dev_id.ToString(), Name = dev.dev_name };
 
-                    if ((dev.dev_name != "Einspeisung") && (dev.dev_name != "Tag"))
+                    if ((dev.dev_name != "Einspeisung") && (dev.dev_name != "Tag") && (!dev.dev_name.ToUpper().Contains("SYS-")))
                         lstDevices.Add(pDevice);
                 }
 
@@ -64,13 +65,17 @@ namespace WebApplication.Controllers
 
             }
 
+            if (String.IsNullOrEmpty(device) || device == "*")
+                return getUsageComplete(dtStart, dtEnd);
+
 
             List<UsageDetail> lstResult = new List<UsageDetail>();
+            
 
             using (KTBDataManagerEntities context = new KTBDataManagerEntities())
             {
                 var usageList = from usage in context.view_dailyUsage
-                                where usage.UNIT == "kWh" &&
+                                where usage.UNIT.ToLower() == "kwh" &&
                                       usage.use_date >= dtStart.Date &&
                                       usage.use_date <= dtEnd.Date &&
                                       usage.DEVICE == device
@@ -101,6 +106,46 @@ namespace WebApplication.Controllers
  
                 }
 
+            }
+
+            return Json(lstResult, JsonRequestBehavior.AllowGet);
+        }
+
+        private JsonResult getUsageComplete(DateTime startDate, DateTime endDate)
+        {
+
+            List<UsageDetail> lstResult = new List<UsageDetail>();
+
+            using (KTBDataManagerEntities context = new KTBDataManagerEntities())
+            {
+                var usageList = from u in context.view_dailyUsage
+                               where  u.UNIT.ToLower() == "kwh"
+                                      && u.use_date >= startDate.Date
+                                      && u.use_date <= endDate.Date
+                               group u by new { u.use_date } into g
+                               select new
+                               {
+                                   Date = g.Key.use_date,
+                                   Value = g.Sum(p => p.VALUE)
+
+                               };
+
+                foreach (var use in usageList)
+                {
+                    UsageDetail detail = new UsageDetail();
+                   
+                    detail.UsageDate = use.Date;
+                    detail.StrUsageDate = use.Date.ToString("dd.MM.yyyy");
+
+                    if (use.Value.HasValue)
+                        detail.Value = use.Value.Value;
+
+                    detail.Location = "";
+
+                    lstResult.Add(detail);
+
+
+                }
             }
 
             return Json(lstResult, JsonRequestBehavior.AllowGet);
